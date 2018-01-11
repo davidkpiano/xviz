8f6a68af3f83f25fdab7fab8a66695a03eb67ec7
David Khourshid <davidkpiano@gmail.com>
2018-01-10 23:57:28 -0500

    Initial commit

---
+++ test/xviz.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/xviz'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
